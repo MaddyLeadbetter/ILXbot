ffffafe8d1dec106e7141abd5cff50746884787d
Maddy <maddyleadbetter@rogers.com>
2018-07-27 17:47:48 -0400

    fix new lines

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ bot.dialog('/', function (session) {
             session.send(`Set your reminder to ${remember}`);
         }
         else if (args[1] === 'help') {
-            session.send('Commands:\nemoji [emoji_name]: returns matching emojis\nset-reminder [remind me of...]: sets a reminder for you\nget-reminder: returns your reminder you set\n');
+            session.send('Commands:\n\nemoji [emoji_name]: returns matching emojis\n\nset-reminder [remind me of...]: sets a reminder for you\n\nget-reminder: returns your reminder you set\n');
         }
         else {
             const messageData = JSON.stringify(args);
